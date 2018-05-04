08e164b1244044a51899f9e82fe351e9b1ce15d3
Carmen <carmenmok1697@gmail.com>
2018-05-04 09:25:52 -0600

    Agregué dos import (mis clases)

--- tec/ic/ia/p1/main.py
+++ tec/ic/ia/p1/main.py
@@ -10,6 +10,8 @@ from Normalizer import Normalizer
 from SVMClassifier import SVMClassifier
 from DecisionTree import DecisionTree
 from KDTree import Kd_Tree
+from logisticRegression import logistic_regression_classifier
+from neuralNetwork import neural_network_classifier
 from tec.ic.ia.pc1.g06 import (
     generar_muestra_pais,
     generar_muestra_provincia
