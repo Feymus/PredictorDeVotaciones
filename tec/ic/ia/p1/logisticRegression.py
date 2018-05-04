8dfbffe1d18925593926491108dff2332c1da51f
Carmen <carmenmok1697@gmail.com>
2018-05-04 16:25:45 -0600

    TTwTT

--- tec/ic/ia/p1/logisticRegression.py
+++ tec/ic/ia/p1/logisticRegression.py
@@ -4,10 +4,12 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from Normalizer import Normalizer
+import copy
 from tec.ic.ia.pc1.g06 import (
     generar_muestra_pais,
     generar_muestra_provincia
 )
+
 
 '''Lista con los Partidos Políticos'''
 political_party = ['ACCESIBILIDAD SIN EXCLUSION','ACCION CIUDADANA',
@@ -17,16 +19,25 @@ political_party = ['ACCESIBILIDAD SIN EXCLUSION','ACCION CIUDADANA',
                    'REPUBLICANO SOCIAL CRISTIANO','RESTAURACION NACIONAL',
                    'UNIDAD SOCIAL CRISTIANA','NULO','BLANCO']
 
-
 class logistic_regression_classifier(object):
 
     '''Clasificador de Modelos de Regresion Logistica'''
-    
+  
 
-    def __init__(self, l_regulizer):
+    def __init__(self, l_regulizer, classes):
         self.l_regulizer = l_regulizer
         self.X = None
         self.y = None
+        self.W = None
+        self.b = None
+        self.y_ = None
+        self.oneHot = OneHotEncoder()
+        allClasses = copy.copy(classes)
+        allClasses = self.replace_political_party(allClasses).reshape(-1, 1)
+        self.oneHot.fit(allClasses)
+
+    def __init__(self):
+        pass
 
     """
     Cambia los partidos politicos por un numero, el cual va a ser el indice que tienen en la
@@ -34,7 +45,7 @@ class logistic_regression_classifier(object):
     Entradas: Lista con los nombres del partido
     Salida: Lista con los partidos cambiados a numeros
     """
-    def replace_political_party(party):
+    def replace_political_party(self, party):
         for i in range(len(party)):
             party[i] = political_party.index(party[i])
         return party
@@ -54,11 +65,14 @@ class logistic_regression_classifier(object):
 
         train_x = data["trainingFeatures"]
         train_y = data["trainingClasses"]
+        test_x = data["testingFeatures"]
+        test_y = data["testingClasses"]
 
-        oneHot = OneHotEncoder()
-        train_y = replace_political_party(train_y).reshape(-1,1)
-        oneHot.fit(train_y)
-        train_y = oneHot.transform(train_y).toarray()
+        train_y = self.replace_political_party(train_y).reshape(-1,1)
+        train_y = self.oneHot.transform(train_y).toarray()
+
+        test_y = self.replace_political_party(test_y).reshape(-1,1)
+        test_y = self.oneHot.transform(test_y).toarray()
 
         shape_x = train_x.shape[1]
         shape_y = train_y.shape[1]
@@ -77,8 +91,8 @@ class logistic_regression_classifier(object):
 
         with tf.name_scope("calculating_cost"):
             # calculando costo
-            cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y,
-                                                                             logits=y_))
+            cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.y,
+                                                                             logits=self.y_))
 
         with tf.name_scope("regulizer"):
             if (self.l_regulizer == 1):
@@ -117,15 +131,47 @@ class logistic_regression_classifier(object):
     ##                    print("Epoch: {}".format(epoch + 1), "cost={}".format(cost_in_each_epoch))
 
 
-        def classify(self,data):
-            test_x = data["testingFeatures"]
-            test_y = data["testingClasses"]
-
-            oneHot = OneHotEncoder()
-            test_y = replace_political_party(test_y).reshape(-1,1)
-            test_y = oneHot.transform(test_y).toarray()
-            
-            return toparty(self.y.eval({self.X: test_x, self.y: test_y}).tolist())
-            
-
+    def classify(self,data):
+        test_x = data["testingFeatures"]
+        test_y = data["testingClasses"]
         
+        test_y = test_y.reshape(-1,1)
+        test_y = self.oneHot.transform(test_y).toarray()
+
+        with tf.name_scope("starting_tensorflow_session"):
+            with tf.Session() as sess:
+                # inicializa las variables
+                sess.run(tf.global_variables_initializer())
+                for epoch in range(epochs):
+                    cost_in_each_epoch = 0
+                    # empieza el entrenamiento
+                    _, c = sess.run([optimizer, cost], feed_dict={self.X: train_x,
+                                                                  self.y: train_y})
+                    cost_in_each_epoch += c
+    ##                # you can uncomment next two lines of code for printing cost when training
+    ##                if (epoch+1) % display_step == 0:
+    ##                    print("Epoch: {}".format(epoch + 1), "cost={}".format(cost_in_each_epoch))
+
+                print("Accuracy Training:", accuracy.eval({X: train_x, y: train_y}))
+##        sess = tf.Session()
+##        with sess.as_default():
+##            return self.toparty(self.y.eval({self.X: test_x, self.y: test_y}).tolist())
+##            
+
+
+##samples = generar_muestra_pais(100)
+##quantity_for_testing = int(100*0.2)
+##normalizer = Normalizer()
+##data = normalizer.prepare_data(samples, quantity_for_testing)
+##classes = np.append(
+##        data["trainingClassesFirst"],
+##        data["testingClassesFirst"],
+##        axis=0
+##    )
+##sample = { "trainingFeatures": data["trainingFeatures"], "trainingClasses": data["trainingClassesFirst"],"testingFeatures": data["testingFeatures"], "testingClasses": data["testingClassesFirst"]}
+##sample2 = { "testingFeatures": data["testingFeatures"], "testingClasses": data["testingClassesFirst"]}
+##print(sample2["testingClasses"])
+##prueba = logistic_regression_classifier(1,classes)
+##prueba.train(sample)
+##print(prueba.classify(sample2))
+
--- tec/ic/ia/p1/neuralNetwork.py
+++ tec/ic/ia/p1/neuralNetwork.py
@@ -7,7 +7,15 @@ from tec.ic.ia.pc1.g06 import (
     generar_muestra_pais,
     generar_muestra_provincia
 )
-import logisticRegression
+from logisticRegression import logistic_regression_classifier
+
+'''Lista con los Partidos Políticos'''
+political_party = ['ACCESIBILIDAD SIN EXCLUSION','ACCION CIUDADANA',
+                   'ALIANZA DEMOCRATA CRISTIANA','DE LOS TRABAJADORES',
+                   'FRENTE AMPLIO','INTEGRACION NACIONAL','LIBERACION NACIONAL',
+                   'MOVIMIENTO LIBERTARIO','NUEVA GENERACION','RENOVACION COSTARRICENSE',
+                   'REPUBLICANO SOCIAL CRISTIANO','RESTAURACION NACIONAL',
+                   'UNIDAD SOCIAL CRISTIANA','NULO','BLANCO']
 
 class neural_network_classifier(object):
 
@@ -21,38 +29,64 @@ class neural_network_classifier(object):
 
     def train(self, data):
 
-        train_x = data["trainingFeatures"]
-        train_y = data["trainingClasses"]
+        x_train = data["trainingFeatures"]
+        y_train = data["trainingClasses"]
+        x_test = data["testingFeatures"]
+        y_test = data["testingClasses"]
         
         oneHot = OneHotEncoder()
-        y_train = logisticRegression.replace_political_party(y_train).reshape(-1,1)
+        var = logistic_regression_classifier()
+        y_train = var.replace_political_party(y_train).reshape(-1,1)
         oneHot.fit(y_train)
         y_train = oneHot.transform(y_train).toarray()
 
+        y_test = var.replace_political_party(y_test).reshape(-1,1)
+        y_test = oneHot.transform(y_test).toarray()
+
         # create model
-        model = Sequential()
+        self.model = Sequential()
         # input layer
-        model.add(Dense(12, input_dim=x_train.shape[1], activation='relu'))
+        self.model.add(Dense(12, input_dim=x_train.shape[1], activation='relu'))
         # hidden layers
-        for i in range(int(self.init_per_layer)):
-            model.add(Dense(self.layers[i], activation=self.activation_func))
+        for i in range(int(self.layers)):
+            self.model.add(Dense(self.unit_per_layer[i], activation=self.activation_func))
         # output layer
-        model.add(Dense(y_train.shape[1], activation='sigmoid'))
+        self.model.add(Dense(y_train.shape[1], activation='softmax'))
 
         # Compile model
-        model.compile(loss='categorical_crossentropy',
+        self.model.compile(loss='mean_squared_error',
                       optimizer='adam',
                       metrics=['accuracy'])
 
         # Fit the model
-        results = model.fit(x_train, y_train,
-                            epochs=150,
-                            batch_size=10,
-                            verbose=0)
+        results = self.model.fit(x_train, y_train,
+                            epochs=300,
+                            batch_size=5,
+                            verbose=0,
+                            validation_data=(x_test, y_test))
 
     def classify(self, data):
         x_test = data["testingFeatures"]
         
-        predictions = model.predict_classes(x_test)
-        return logisticRegression.political_party[predictions[0]])
-        
+        predictions = self.model.predict_classes(x_test)
+        print(self.model.predict(x_test))
+        print(predictions)
+        return political_party[predictions[0]]
+
+
+samples = generar_muestra_pais(100)
+quantity_for_testing = int(100*0.2)
+normalizer = Normalizer()
+data = normalizer.prepare_data(samples, quantity_for_testing)
+classes = numpy.append(
+        data["trainingClassesFirst"],
+        data["testingClassesFirst"],
+        axis=0
+    )
+sample = { "trainingFeatures": data["trainingFeatures"], "trainingClasses": data["trainingClassesFirst"],"testingFeatures": data["testingFeatures"], "testingClasses": data["testingClassesFirst"]}
+sample2 = { "testingFeatures": data["testingFeatures"], "testingClasses": data["testingClassesFirst"]}
+
+pruebas = neural_network_classifier(10,[1,2,3,4,5,2,3,10,23,10],'sigmoid')
+pruebas.train(sample)
+print(pruebas.classify(sample2))
+
